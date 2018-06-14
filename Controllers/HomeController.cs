51829c03f01a8d7905fc7a92a688a0f074dfc381
davea21 <davesidmouth@gmail.com>
2018-06-14 11:32:16 +0100

    start

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext _Context;
+        public HomeController(DataContext context)
+        {
+            _Context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_Context.Products.First());
+        }
+
+        
+    }
+}
---
+++ FBContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore
+{
+    public class DBContextFactory : IDesignTimeDbContextFactory<DataContext>
+        {
+        public DataContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-SRELHR1\SQLEXPRESS;Initial Catalog=SportsStore;Integrated Security=True");
+
+                return new DataContext(optionsBuilder.Options);
+            }
+        }
+    
+}
